6146e8abc070b8bbef73afb81e4c05fd2de8d7e6
Arthur Loza <loza.uns@gmail.com>
2017-07-21 13:02:25 +0300

    HW12_forms: final

--- scripts.js
+++ scripts.js
@@ -1,27 +1,35 @@
+var username = document.forms.authorization.elements.username;
+var password = document.forms.authorization.elements.password;
+var submit = document.forms.authorization.elements.submit;
 
+var regUsername = /^[a-z0-9]{2,15}$/i;
+var regPassword = /^(?=.*[$])(?=.*[#])[a-z0-9#$]{6,15}$/i;
 
+submit.onclick = function() {
+    var usernameValid = regUsername.test(username.value);
+    var passwordValid = regPassword.test(password.value);
 
+    username.classList.remove("invalid");
+    password.classList.remove("invalid");
 
+    if ( usernameValid === true && passwordValid === true ) {
+        checkLocalStorage();
+    } else {
+        if ( usernameValid === false ) {
+            username.classList.add("invalid");
+        }
+        if ( passwordValid === false ) {
+            password.classList.add("invalid");
+        }
+    }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function saveData() {
-    var username = document.getElementById("username");
-    var password = document.getElementById("password");
-    localStorage.setItem("username", username.value);
-    localStorage.setItem("password", password.value);
+function checkLocalStorage() {
+    if (typeof localStorage !== 'undefined') {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+    } else {
+        document.cookie = 'username=' + username.value;
+        document.cookie = 'password=' + password.value;
+    }
 }
